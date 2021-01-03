9707cb4e3ff47eae4ca984301d4683e161954fc4
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2021-01-02 22:24:34 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,19 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    if (title.length == 0) {
+    return title;
+  } else if (title.length == 1 && title.charAt(0) ==
+         title.charAt(0).toUpperCase()) {
+    return title;
+  } else {
+    let arr = title.split('');
+    arr[0] = arr[0].toUpperCase();
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i-1] == ' ') {
+        arr[i] = arr[i].toUpperCase();
+      }
+    }
+    return arr.join('');
+  }
 }
 
 // Do not edit this line;
