ef7004f5910e325e9dfebfe659c3d0cf25aa5776
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2021-01-02 22:36:52 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let isPalindrome = true;
+  for (let i = 0; i <= word.length/2; i++) {
+    if ( word.charAt(i) != word.charAt(word.length - i - 1) ) {
+      isPalindrome = false;
+    }
+  }
+  return isPalindrome;
 }
 
 // Do not edit this line;
