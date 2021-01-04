1d1f7cf0de5389476d00d20056dc40bb5dc4a574
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2021-01-04 00:15:43 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,11 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let union = new Set();
+  args.forEach( function (S) {
+    return S.forEach( function (e) {
+      return union.add(e)
+    })
+  })
+  return union;
 }
 
 // Do not edit this line;
