3fe6e948e64ada002a73f054dab7040f3548f8bc
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2021-01-04 00:31:06 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,19 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if (nums.length < 2) {
+    throw 'Array must have length 2 or greater.'
+  }
+  try {
+    for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = i+1; j < nums.length; j++) {
+        if (nums[i]+nums[j] == target) {
+          return true;
+        }
+      }
+    }
+    return false;
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 // Do not edit this line;
